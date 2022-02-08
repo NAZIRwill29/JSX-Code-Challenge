a6e4e835b024ffae080a851a41674a84d7cf0a99
NAZIRwill29 <59091291+NAZIRwill29@users.noreply.github.com>
2022-02-08 16:07:59 +0800

    finish

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>JSX Challenge</h1>
+    <ul>
+      <li>hey</li>
+      <li>hey</li>
+      <li>hey</li>
+    </ul>
+  </div>,
+  document.querySelector("#root")
+);
